3d7017f2202f746615a28cf7577aedbea1280bdd
Pinkkinggze <quentin1.payet@epitech.eu>
2022-07-25 14:02:16 +0400

    mise en ligne coming soon

--- mail.php
+++ mail.php
@@ -1,8 +1,8 @@
 <?php
     header('Location: https://pinkkinggze.github.io/onarrive.github.io/');
-    if (isset($_GET["mail"]) != "") {
-        $handle = fopen("./client.txt", "a");
-        fwrite($handle, $_POST["mail"] . "\n");
-        fclose($handle);
-    }
+    // if (isset($_GET["mail"]) != "") {
+    //     $handle = fopen("./client.txt", "a");
+    //     fwrite($handle, $_POST["mail"] . "\n");
+    //     fclose($handle);
+    // }
 ?>
