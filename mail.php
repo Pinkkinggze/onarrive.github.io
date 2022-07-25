eb4980722a7b05100bb447647b4985f799e1b548
Pinkkinggze <quentin1.payet@epitech.eu>
2022-07-25 14:07:34 +0400

    Delete mail.php

--- mail.php
+++
@@ -1,8 +0,0 @@
-<?php
-    header('Location: https://pinkkinggze.github.io/onarrive.github.io/');
-    // if (isset($_GET["mail"]) != "") {
-    //     $handle = fopen("./client.txt", "a");
-    //     fwrite($handle, $_POST["mail"] . "\n");
-    //     fclose($handle);
-    // }
-?>
